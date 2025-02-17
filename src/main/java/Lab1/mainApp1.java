2a58ebc8ec0d2fc01909654e2a3a1eed25bef940
Andy ZENG <yzengay@connect.ust.hk>
2025-02-17 16:52:28 +0800

    Add 1 more line to record my progress

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,7 +5,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
-        System.out.println("Program...Starting...");
+        System.out.println("I'm learning how to use IntelliJ and Github. Nice!");
+        System.out.println("Program ..Starting...");
         System.out.println("1 + 1 = "+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
